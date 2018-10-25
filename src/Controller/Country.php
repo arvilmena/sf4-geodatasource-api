a646c1c42208b044e226cf480b234bf5eeaba75e
Arvil Meña <arvilmena@gmail.com>
2018-10-25 09:14:35 +0800

    Country API

--- src/Controller/Country.php
+++ src/Controller/Country.php
@@ -4,11 +4,29 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class Country extends AbstractController
 {
     public function index()
     {
-        return new Response('hello world!');
+        
+        $encoders = array(new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+
+        $serializer = new Serializer($normalizers, $encoders);
+        
+        $countries = $this->getDoctrine()
+            ->getRepository(\App\Entity\Country::class)->findAll();
+
+        $jsonContent = $serializer->serialize($countries, 'json', array('attributes' => array('ccIso', 'ccFips', 'countryName')));
+
+        $response = new Response();
+        $response->setContent( $jsonContent );
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 }
