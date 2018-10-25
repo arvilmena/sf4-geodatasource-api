54363ab00f9f8f3b663f0998f4a6d03959104a6c
Arvil @DesktopPC <arvilmena@gmail.com>
2018-10-25 19:40:58 +0800

    doctrine:migrations:diff

---
+++ src/Migrations/Version20181025113929.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181025113929 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE city ADD cc_iso VARCHAR(2) NOT NULL');
+        $this->addSql('ALTER TABLE country CHANGE tld tld VARCHAR(3) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE city DROP cc_iso');
+        $this->addSql('ALTER TABLE country CHANGE tld tld VARCHAR(3) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci');
+    }
+}
